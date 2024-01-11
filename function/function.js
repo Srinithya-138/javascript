087b10eaa318010cbddff6529191d6433e0a0ec9
Srinithya-138 <srinithyam2001@gmail.com>
2024-01-11 16:49:31 +0530

    tdy

--- function/function.js
+++ function/function.js
@@ -56,3 +56,43 @@ let countVowels=(str)=>{
     return count;
 }
 console.log(countVowels("srinithya"))
+
+let arr=[1,2,3,45,4];
+
+let sum=(a,b)=>{
+ return a+b;
+}
+let x=sum(4,5)
+console.log(x);
+let mul=(a,b)=>{
+    return a*b;
+}
+let y=mul(1,5);
+console.log(y);
+
+let area=(l,b)=>{
+    return l*b;
+}
+let a=area(3,4);
+console.log(a);
+
+let inpt=(str)=>{
+    let count=0;
+    for(let char of str){
+        if(char=="a"||char=="e"||char=="i"||char=="o"||char=="u"){
+            count++;
+        }
+    }
+    console.log(count);
+}
+inpt("aeiou");
+console.log("for each");
+let ar=[1,2,3,4,5,6];
+// ar.forEach(function print(val ,index){
+//     console.log(`index is ${index}`);
+//     console.log(`value is ${val}`);
+// })
+ar.forEach( print=(val ,index)=>{
+        console.log(`index is ${index}`);
+        console.log(`value is ${val}`);
+     })
---
+++ practiceset/practise.js
@@ -0,0 +1,14 @@
+// let fullName=prompt("enter your full name");
+// console.log("@"+fullName+fullName.length);
+
+// let arr=["blooberg","microsoft","uber","google","IBM","netflix"];
+// console.log("when deleted the first company from array ",arr.shift());
+// console.log("when we replace uber and add ola in its place ",arr.splice(2,1,"ola"));
+// console.log("add amazon at the end ",arr.push("amazon"));
+
+//console.log(arr[i]);
+let ar=[1,2,3,4,5,6];
+ar.forEach((val)=>{
+    val=val*val;
+    console.log(val);
+})
