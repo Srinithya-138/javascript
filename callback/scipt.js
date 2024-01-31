90491516b304e95a66566267cbab5f187862de30
Srinithya-138 <srinithyam2001@gmail.com>
2024-01-31 16:50:03 +0530

    tdy

---
+++ callback/scipt.js
@@ -0,0 +1,12 @@
+// function calculator(a,b,sumCallBack){
+//     sumCallBack(a,b)
+// }
+// calculator(2,3,(a,b)=>{
+//     console.log(a+b)
+// })
+
+const hello=()=>{
+    console.log("hello")
+}
+
+setTimeout(hello,3000)
